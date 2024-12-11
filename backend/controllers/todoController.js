94195cb8be1b582a9417813546c5b1db301a25d6
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-12-11 20:02:00 +0530

    Backend

---
+++ backend/controllers/todoController.js
@@ -0,0 +1,22 @@
+const Todo = require("../models/todoModel");
+
+exports.getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createTodo = async (req, res) => {
+  try {
+    const newTodo = new Todo({
+      text: req.body.text,
+    });
+    await newTodo.save();
+    res.json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
